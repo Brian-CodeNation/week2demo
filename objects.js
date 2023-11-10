2a56600e011551bbce339112a1ff8ed8aebaeedf
Brian-CodeNation <brian.harkins@wearecodenation.com>
2023-11-10 10:07:30 +0000

    added functions.js file

---
+++ objects.js
@@ -0,0 +1,76 @@
+let person = {
+    name: "Brian",
+    age: 38
+};
+
+// console.log(person.age);
+// console.log(person["age"]);
+
+// let offer = "none";
+// let time = 1700;
+
+// const cafe = {
+//     name: "Whitesheep",
+//     seatingCapacity: 100,
+//     hasSpecialOffers: true,
+//     drinks: ["Cappuccino", "Latte", "Filter Coffee", "Tea", "Hot Chocolate"],
+//     breakfastOffer: "Free croissant with coffee",
+//     lunchOffer: "Free drink with surprisingly priced sandwich",
+//     noOffer: "Sorry - no offer"
+// };
+
+// if (time < 1100) {
+//     offer = cafe.breakfastOffer;
+//     console.log(cafe.breakfastOffer);
+// } else if (time < 1500) {
+//     offer = cafe.lunchOffer;
+//     console.log(cafe.lunchOffer);
+// }
+
+// let day = "Monday";
+
+// const alarm = {
+//     weekendAlarm: "No alarm needed.",
+//     weekdayAlarm: "Get up at 7am."
+// };
+
+// if (day == "Sunday" || day == "Saturday") {
+//     console.log(alarm.weekendAlarm);
+// } else {
+//     console.log(alarm.weekdayAlarm);
+// }
+
+// let person = {
+//     name: "Brian",
+//     age: 38,
+//     favSongs: ["Oops I did it again - Britney", "Hysteria - Muse", "Sleeptalking - Level 42"]
+// };
+
+// person.favSongs[2] = "Jigsaw - Radiohead";
+// console.log(person);
+
+// let offer = "none";
+// let time = 12700;
+
+// const cafe = {
+//     name: "Whitesheep",
+//     seatingCapacity: 100,
+//     hasSpecialOffers: true,
+//     drinks: ["Cappuccino", "Latte", "Filter Coffee", "Tea", "Hot Chocolate"],
+//     breakfastOffer: "Free croissant with coffee",
+//     lunchOffer: "Free drink with surprisingly priced sandwich",
+//     noOffer: "Sorry - no offer",
+    
+//     openCafe(){
+//         if (this.hasSpecialOffers){
+//             return "Time for a special offer";
+//         }
+//     },
+
+//     closeCafe(){
+//         return "We are closed. Come back tomorrow";
+//     }
+// };
+
+// console.log(cafe.openCafe());
+
